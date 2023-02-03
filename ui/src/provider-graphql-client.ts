92cd7577c07657965f73d92f4a8bcc61391dea18
pospi <pospi@spadgos.com>
2023-02-03 16:54:11 +1000

    omit enabledVFModules from client init; allow it to bind to whatever is present

--- ui/src/provider-graphql-client.ts
+++ ui/src/provider-graphql-client.ts
@@ -1,19 +1,8 @@
 import initGraphQLClient, { ClientOptions } from '@vf-ui/graphql-client-holochain'
-import { VfModule } from '@valueflows/vf-graphql-holochain'
 import { ApolloClient, NormalizedCacheObject } from '@apollo/client/core'
 
 export const provideGraphQLClient = (opts: ClientOptions): Promise<ApolloClient<NormalizedCacheObject>> =>
-  initGraphQLClient({
-    ...opts,
-    enabledVFModules: [
-      VfModule.Observation,
-      VfModule.Measurement,
-      VfModule.Agent,
-      VfModule.Action,
-      VfModule.ResourceSpecification,
-      VfModule.ProcessSpecification,
-    ],
-  })
+  initGraphQLClient(opts)
   .then(client => {
     // :SHONK: Assign window global used by @apollo-elements/core controllers.
     //         Due to https://github.com/lit/lit/issues/2446 such controllers can't be
