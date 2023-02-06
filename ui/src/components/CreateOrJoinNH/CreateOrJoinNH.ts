5b231c66f5bbf3d666e922b166edaa2ce4ccaa34
pospi <pospi@spadgos.com>
2023-02-06 11:54:48 +1000

    fix indentation

--- ui/src/components/CreateOrJoinNH/CreateOrJoinNH.ts
+++ ui/src/components/CreateOrJoinNH/CreateOrJoinNH.ts
@@ -7,50 +7,50 @@ import { TextField, Button } from '@scoped-elements/material-web'
 const Scoped = dedupeMixin(ScopedElementsMixin)
 
 export class CreateOrJoinNh extends Scoped(LitElement) {
-    @query('#ca-pubkey')
-    input!: HTMLInputElement;
+  @query('#ca-pubkey')
+  input!: HTMLInputElement;
 
-    render() {
-        return html`
-            <div class="nh-creation-container">
-                <mwc-button outlined=true @click=${this.dispatchCreateNeighbourhood}>Create Neighbourhood</mwc-button>
-                <div>
-                    <mwc-textfield id="ca-pubkey" placeholder=${`community activator pubkey`}></mwc-textfield>
-                    <mwc-button outlined=true @click=${this.dispatchJoinNeighbourhood}>Join Neighbourhood</mwc-button>
-                </div>
-            </div>
-        `
-    }
-
-    dispatchCreateNeighbourhood() {
-        this.dispatchEvent(new CustomEvent('create-nh'))
-    }
-
-    dispatchJoinNeighbourhood() {
-        const newValue = this.input.value;
-        if (newValue) {
-            const options = {
-                detail: {newValue},
-                bubbles: true,
-                composed: true
-            };
-            console.log('ca key', newValue)
-            this.dispatchEvent(new CustomEvent('join-nh', options))
-            this.input.value = ''
-        }
-    }
-
-    static get scopedElements() {
-        return {
-            'mwc-textfield': TextField,
-            'mwc-button': Button,
-        }
-    }
-
-    static styles = css`
-        .nh-creation-container {
-            display: flex;
-            flex-direction: column;
-        }
+  render() {
+    return html`
+      <div class="nh-creation-container">
+        <mwc-button outlined=true @click=${this.dispatchCreateNeighbourhood}>Create Neighbourhood</mwc-button>
+        <div>
+          <mwc-textfield id="ca-pubkey" placeholder=${`community activator pubkey`}></mwc-textfield>
+          <mwc-button outlined=true @click=${this.dispatchJoinNeighbourhood}>Join Neighbourhood</mwc-button>
+        </div>
+      </div>
     `
+  }
+
+  dispatchCreateNeighbourhood() {
+    this.dispatchEvent(new CustomEvent('create-nh'))
+  }
+
+  dispatchJoinNeighbourhood() {
+    const newValue = this.input.value;
+    if (newValue) {
+      const options = {
+        detail: {newValue},
+        bubbles: true,
+        composed: true
+      };
+      console.log('ca key', newValue)
+      this.dispatchEvent(new CustomEvent('join-nh', options))
+      this.input.value = ''
+    }
+  }
+
+  static get scopedElements() {
+    return {
+      'mwc-textfield': TextField,
+      'mwc-button': Button,
+    }
+  }
+
+  static styles = css`
+    .nh-creation-container {
+      display: flex;
+      flex-direction: column;
+    }
+  `
 }
