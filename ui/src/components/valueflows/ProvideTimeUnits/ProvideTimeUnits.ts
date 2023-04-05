bba9518cb56c9474a4eb2d6fd4ae84817fccf867
pospi <pospi@spadgos.com>
2023-04-05 14:43:18 +1000

    fix mishandling of cache updates when initialising time-related Unit defs

--- ui/src/components/valueflows/ProvideTimeUnits/ProvideTimeUnits.ts
+++ ui/src/components/valueflows/ProvideTimeUnits/ProvideTimeUnits.ts
@@ -58,6 +58,13 @@ export interface ITimeUnits {
   seconds: Unit,
 }
 
+const coreUnitsQueryVars = {
+  // :TODO: update to use query API
+  hours: 'h',
+  minutes: 'min',
+  seconds: 's',
+}
+
 export class ProvideTimeUnits extends ScopedElementsMixin(LitElement)
 {
   private slottedChildren: HTMLElement[] = []
@@ -65,21 +72,20 @@ export class ProvideTimeUnits extends ScopedElementsMixin(LitElement)
   _initialisedUnitRecords = false
 
   units: ApolloQueryController<CoreUnitsCheckResponse> = new ApolloQueryController(this, HasCoreUnits, {
-    variables: {
-      // :TODO: update to use query API
-      hours: 'h',
-      minutes: 'min',
-      seconds: 's',
-    }
+    variables: coreUnitsQueryVars,
   })
 
   registerUnits: ApolloMutationController<RegisterCoreUnitsResponse> = new ApolloMutationController(this, RegisterTimeUnits, {
     // refresh `units` read query when write request completes
-    update: (cache, result) => {
+    update: (cache, { data }) => {
       cache.writeQuery({
         query: HasCoreUnits,
-        data: { ...result.data },
-        overwrite: true,
+        variables: coreUnitsQueryVars,
+        data: {
+          hours: data?.hours.unit,
+          minutes: data?.minutes.unit,
+          seconds: data?.seconds.unit,
+        },
       })
       this.units.subscribe({ fetchPolicy: 'cache-only' })
     },
@@ -101,7 +107,7 @@ export class ProvideTimeUnits extends ScopedElementsMixin(LitElement)
     const noUnits = hasEmptyUnits(this.units)
     if (noUnits && !this._initialisedUnitRecords) {
       this._initialisedUnitRecords = true
-      this.initialiseUnitRecords()
+      await this.initialiseUnitRecords()
     }
 
     /// Propagate updates to `units` from API to `timeUnitDefs` property of any currently rendered children
